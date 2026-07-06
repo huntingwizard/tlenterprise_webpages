945c95790f4a2380a61f34f858d461c61adb2f51
huntingwizard <shadethecommander11@gmail.com>
2026-07-06 00:25:00 -0400

    praying

--- src/TopNavigator.jsx
+++ src/TopNavigator.jsx
@@ -1,8 +1,29 @@
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
+function TopNavigator() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/#/">TL Enterprise LLC</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/app">Accounting</Nav.Link>
+            <Nav.Link as={Link} to="/app1">IT Counseling</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TopNavigator;
+/** 
 function TopNavigator() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
@@ -21,4 +42,4 @@ function TopNavigator() {
   );
 }
 
-export default TopNavigator;
+export default TopNavigator;*/
