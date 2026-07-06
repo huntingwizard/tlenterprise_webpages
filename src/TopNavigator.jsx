9e2c2249073a7a61ad4adc118f1a4c9e504b8319
huntingwizard <shadethecommander11@gmail.com>
2026-07-06 00:19:14 -0400

    fixed homepage route

--- src/TopNavigator.jsx
+++ src/TopNavigator.jsx
@@ -11,7 +11,7 @@ function TopNavigator() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">TL Enterprise LLC</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">TL Enterprise LLC</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
